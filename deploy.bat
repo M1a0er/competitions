31a802fdbdf53c08c655a41479823d01f7b96f20
M1a0er <M1a0er@users.noreply.github.com>
2026-08-07 17:03:08 +0800

    update

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,6 @@
-@echo off
+
 cd /d C:\Users\miaoer\Desktop\web_competition
-"C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m "更新: %date% %time%"
-"C:\Program Files\Git\bin\git.exe" push origin main
-echo ========== 部署完成 ==========
+git add .
+git commit -m "update"
+git push origin
 pause
